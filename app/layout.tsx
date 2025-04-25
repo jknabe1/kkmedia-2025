da9e7f5d9a5f52d62cc985d34b012093c1b112df
jknabe1 <101703981+jknabe1@users.noreply.github.com>
2025-04-25 12:20:39 +0200

    refactor: Clean up globals.css and layout.tsx for improved structure

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Footer from "./components/Global/Footer";
+import ContactSection from "./components/Global/ContactSection";
+import Header from "./components/Global/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,10 +27,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Header />
         {children}
+        <ContactSection/>
+        <Footer />
       </body>
     </html>
   );
